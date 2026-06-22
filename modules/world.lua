071ceec1bd3af3ccac710ae6a042683cf9181e07
thepro2324 <orikiiancilevich2012@gmail.com>
2026-06-22 18:00:12 +0300

    Add WorldMod with gravity and FOV functions

--- modules/world.lua
+++ modules/world.lua
@@ -1,1 +1,14 @@
+-- modules/world.lua
+local WorldMod = {}
+local workspace = game:GetService("Workspace")
+local camera = workspace.CurrentCamera
 
+function WorldMod.setGravity(value)
+    workspace.Gravity = value
+end
+
+function WorldMod.setFOV(value)
+    camera.FieldOfView = value
+end
+
+return WorldMod
