a5dfab72112743eb1235318f14b796f2eb97bac4
thepro2324 <orikiiancilevich2012@gmail.com>
2026-07-08 02:58:41 +0300

    Update menu.lua

--- ui/menu.lua
+++ ui/menu.lua
@@ -9,15 +9,15 @@ local screen = Instance.new("ScreenGui", CoreGui)
 screen.Name = "MyMenu"
 
 local frame = Instance.new("Frame", screen)
-frame.Size = UDim2.new(0, 200, 0, 150)
+frame.Size = UDim2.new(0, 600, 0, 300)
 frame.Position = UDim2.new(0.5, -100, 0.5, -75)
 frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 Instance.new("UICorner", frame)
 
 local btn = Instance.new("TextButton", frame)
-btn.Size = UDim2.new(0.8, 0, 0, 40)
+btn.Size = UDim2.new(0.9, 0, 0, 40)
 btn.Position = UDim2.new(0.1, 0, 0.3, 0)
-btn.Text = "Kill Aura"
+btn.Text = "HOME"
 btn.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
 btn.TextColor3 = Color3.new(1, 1, 1)
 Instance.new("UICorner", btn)
