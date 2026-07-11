a360cda817d2ed8ff93a48c2ece2e25a08d463e6
thepro2324 <orikiiancilevich2012@gmail.com>
2026-07-12 02:29:58 +0300

    Update player.lua

--- modules/player.lua
+++ modules/player.lua
@@ -4,5 +4,16 @@ function Module.init(parent, Elements)
     Elements.createSlider(parent, "JumpPower", 50, 300, 50, function(v) end)
     Elements.createToggleButton(parent, "Infinite Jump", false, function(s) end)
     Elements.createToggleButton(parent, "Noclip", false, function(s) end)
+
+
+
+function Module.init(parent, Elements)
+    -- כאן אנחנו יוצרים את התוכן של הקטגוריה
+    Elements.createToggleButton(parent, "WalkSpeed Boost", false, function(state)
+        print("WalkSpeed is now: " .. tostring(state))
+    end)
+end
+
+
 end
 return Module
