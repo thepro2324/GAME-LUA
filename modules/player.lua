fbdb16e7b2a678e3495c966927974eca0f62db8e
thepro2324 <orikiiancilevich2012@gmail.com>
2026-07-12 19:06:17 +0300

    Update player.lua

--- modules/player.lua
+++ modules/player.lua
@@ -1,19 +1,9 @@
 local Module = {}
-function Module.init(parent, Elements)
-    Elements.createSlider(parent, "WalkSpeed", 16, 200, 16, function(v) end)
-    Elements.createSlider(parent, "JumpPower", 50, 300, 50, function(v) end)
-    Elements.createToggleButton(parent, "Infinite Jump", false, function(s) end)
-    Elements.createToggleButton(parent, "Noclip", false, function(s) end)
-
-
-
-function Module.init(parent, Elements)
-    -- כאן אנחנו יוצרים את התוכן של הקטגוריה
-    Elements.createToggleButton(parent, "WalkSpeed Boost", false, function(state)
-        print("WalkSpeed is now: " .. tostring(state))
-    end)
-end
-
-
+function Module.init(tab, Elements)
+    Elements.createToggleButton(tab, "WalkSpeed", false, function(s) end)
+    Elements.createSlider(tab, "Speed Value", 16, 200, 16, function(v) end)
+    Elements.createToggleButton(tab, "Infinite Jump", false, function(s) end)
+    Elements.createToggleButton(tab, "Noclip", false, function(s) end)
+    Elements.createButton(tab, "Reset Character", function() end)
 end
 return Module
