e9a7fb7bbfe71f7de682f1f92786c89dc4621bae
thepro2324 <orikiiancilevich2012@gmail.com>
2026-07-12 19:07:16 +0300

    Update world.lua

--- modules/world.lua
+++ modules/world.lua
@@ -1,6 +1,7 @@
 local Module = {}
-function Module.init(parent, Elements)
-    Elements.createToggleButton(parent, "Fullbright", false, function(s) end)
-    Elements.createToggleButton(parent, "No Fog", false, function(s) end)
+function Module.init(tab, Elements)
+    Elements.createSlider(tab, "Time of Day", 0, 24, 12, function(v) end)
+    Elements.createToggleButton(tab, "No Fog", true, function(s) end)
+    Elements.createButton(tab, "Remove Parts", function() end)
 end
 return Module
