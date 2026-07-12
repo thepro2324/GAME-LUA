48585d90a18c73c21dba66139e0a4e35d5a37255
thepro2324 <orikiiancilevich2012@gmail.com>
2026-07-12 19:06:04 +0300

    Update home.lua

--- modules/home.lua
+++ modules/home.lua
@@ -1,8 +1,7 @@
 local Module = {}
-function Module.init(parent, Elements)
-    local title = Instance.new("TextLabel", parent)
-    title.Size = UDim2.new(1, 0, 0, 50); title.Text = "Welcome to script ori_dev_team"
-    title.TextColor3 = Color3.fromRGB(255, 255, 255); title.BackgroundTransparency = 1
-    title.Font = Enum.Font.GothamBold; title.TextSize = 20
+function Module.init(tab, Elements)
+    Elements.createLabel(tab, "Welcome to Ori Hub V12")
+    Elements.createLabel(tab, "Status: Online")
+    Elements.createButton(tab, "Join Discord", function() print("Discord link") end)
 end
 return Module
