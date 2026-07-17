8c9647b0120de0b48351bc2c9b8c609a90f230ed
thepro2324 <orikiiancilevich2012@gmail.com>
2026-07-18 02:49:25 +0300

    Update home.lua

--- modules/home.lua
+++ modules/home.lua
@@ -1,11 +1,11 @@
 local Module = {}
 
 function Module.init(tab, Elements)
-    Elements.createLabel(tab, "Welcome to Ori Hub")
+    Elements.createLabel(tab, "Welcome to Ori Hub V12")
     
     Elements.createButton(tab, "Copy Discord Link", function()
         setclipboard("https://discord.gg/fFwaNja5tj")
-        print("Discord link copied!")
+        print("Discord link copied to clipboard!")
     end)
 end
 
